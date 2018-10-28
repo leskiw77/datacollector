804c7a300e9c4e29e759587aa56d026b089f4465
jarema <jarema.leskiw@gmail.com>
2018-10-28 22:27:52 +0100

    add whole test data

---
+++ stats/collect_time/fitFunction/RegressionFit.py
@@ -0,0 +1,34 @@
+from pandas import DataFrame
+from sklearn import linear_model
+import csv
+
+result_file_path = 'stats/collect_time/fitFunction/results.csv'
+
+Results = {
+    'threads_number': [],
+    'size': [],
+    'collect_time': []
+}
+with open(result_file_path, 'r') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        Results['threads_number'].append(row['threads_number'])
+        Results['size'].append(row['size'])
+        Results['collect_time'].append(row['collect_time'])
+
+df = DataFrame(Results, columns=['threads_number', 'size', 'collect_time'])
+
+X = df[['threads_number',
+        'size']]
+Y = df['collect_time']
+
+# with sklearn
+regr = linear_model.LinearRegression()
+regr.fit(X, Y)
+
+print(regr.score(X, Y))
+print('Intercept: \n', regr.intercept_)
+print('Coefficients: \n', regr.coef_)
+
+x = DataFrame
+regr.predict()
